a91f4b34e4a8a9ba3adda7056946e2164076ba1f
Loir Ricardo Sandoval Martinez <loir.sandoval.29@hotmail.com>
2019-12-08 14:16:46 -0700

    Seeder de otro proyecto agregado

--- database/seeds/ProjectsTableSeeder.php
+++ database/seeds/ProjectsTableSeeder.php
@@ -18,5 +18,12 @@ class ProjectsTableSeeder extends Seeder
         $project->fechaInicio = "2019-09-12";
         $project->fechaCierre = "2019-11-24";
         $project->save();
+
+        $project = new Project();
+        $project->nombre = "Reloj Checador/Asistencia";
+        $project->descripcion = "Crear una plataforma web para llevar el control de las entradas y salidas del taller de desarrollo del DASC";
+        $project->fechaInicio = "2019-11-24";
+        $project->fechaCierre = "2019-12-12";
+        $project->save();
     }
 }
